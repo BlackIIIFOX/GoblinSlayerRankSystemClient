103f1c80421def9f72177eeebcbe99a5caecbeb0
Paul Rozhkin <blackiiifox@gmail.com>
2020-06-13 21:17:09 +0300

    Add form for contract create

--- src/app/create-new-contract/create-new-contract.component.ts
+++ src/app/create-new-contract/create-new-contract.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {ToastService} from '../core/services';
 
 @Component({
   selector: 'app-create-new-contract',
@@ -6,10 +9,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-new-contract.component.css']
 })
 export class CreateNewContractComponent implements OnInit {
+  newContractForm: FormGroup;
 
-  constructor() { }
+  submitted = false;
+  processing = false;
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder,
+              private router: Router,
+              private toastService: ToastService) {
   }
 
+  ngOnInit(): void {
+    this.newContractForm = this.fb.group(
+      {
+        name: ['', [Validators.required]],
+        address: ['', Validators.required],
+        reward: ['', [Validators.required, Validators.min(0), Validators.pattern('\\d*')]],
+        description: ['', Validators.required],
+        comment: ['']
+      });
+  }
+
+  get name(): AbstractControl {
+    return this.newContractForm.get('name');
+  }
+
+  get address(): AbstractControl {
+    return this.newContractForm.get('address');
+  }
+
+  get reward(): AbstractControl {
+    return this.newContractForm.get('reward');
+  }
+
+  get description(): AbstractControl {
+    return this.newContractForm.get('description');
+  }
+
+  get comment(): AbstractControl {
+    return this.newContractForm.get('comment');
+  }
+
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (!this.newContractForm.valid) {
+      return;
+    }
+
+    this.processing = true;
+  }
 }

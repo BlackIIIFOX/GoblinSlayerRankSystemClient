dd01ac46ec4ee0777a4331749872674669fd0ce6
paulrozhkin <blackiiifox@gmai.com>
2020-10-01 18:49:10 +0300

    remove unused component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,6 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {CoreModule} from './core';
 import {AppRoutingModule} from './app-routing.module';
 import {HttpClientModule} from '@angular/common/http';
-import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
-import {InMemUserDataService} from './core/mock';
 import {WelcomeComponent} from './welcome/welcome.component';
 
 import {
@@ -23,7 +21,6 @@ import {DashboardComponent} from './admin-panel/dashboard/dashboard.component';
 import {UserUpdateComponent} from './admin-panel/management-users/user-update/user-update.component';
 import {UserCreateComponent} from './admin-panel/management-users/user-create/user-create.component';
 import {ManagementUsersComponent} from './admin-panel/management-users/management-users.component';
-import {AdventurerRegistrationComponent} from './adventurer-registration/adventurer-registration.component';
 import {RegistrationComponent} from './registration/registration.component';
 import {ToastsComponent} from './toasts/toasts.component';
 import {CreateNewContractComponent} from './create-new-contract/create-new-contract.component';
@@ -48,7 +45,6 @@ import { AccountSettingsComponent } from './account-settings/account-settings.co
     ManagementUsersComponent,
     UserUpdateComponent,
     UserCreateComponent,
-    AdventurerRegistrationComponent,
     RegistrationComponent,
     ToastsComponent,
     CreateNewContractComponent,

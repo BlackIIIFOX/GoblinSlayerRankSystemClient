8cd0480ac41b8ae390eeb9fca7beee127387ce46
Paul Rozhkin <blackiiifox@gmail.com>
2020-10-22 00:10:37 +0300

    search for contracts by an adventurer

--- src/app/all-list-contracts/all-list-contracts.component.ts
+++ src/app/all-list-contracts/all-list-contracts.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {AccountService, ContractsService} from '../core/services';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {AccountService, AdventurersService, ContractsService} from '../core/services';
 import {Observable, Subscription} from 'rxjs';
 import {Contract} from '../core/models/contract.model';
-import {ContractStatus, Rank, Role, User} from '../core/models';
+import {Adventurer, ContractStatus, Rank, Role, User} from '../core/models';
 import {map} from 'rxjs/operators';
 import validate = WebAssembly.validate;
 
@@ -20,9 +20,18 @@ export class AllListContractsComponent implements OnInit, OnDestroy {
   private _contractStatusFilter: ContractStatus;
   // tslint:disable-next-line:variable-name
   private _minRankFilter: Rank;
+  // tslint:disable-next-line:variable-name
+  private _rankFilter: Rank;
   public contractStatus = Object.values(ContractStatus);
   public ranks = Object.values(Rank);
   private accountSubscription: Subscription;
+  // tslint:disable-next-line:variable-name
+  private _showOnlyMyContracts: boolean;
+
+  // Отображать ли список контрактов с фильтрами для авантюриста
+  @Input() isListForAdventurer: boolean;
+
+  @Input() adventurerInfo: Adventurer;
 
   constructor(public contractsService: ContractsService, private accountService: AccountService) {
   }
@@ -42,10 +51,35 @@ export class AllListContractsComponent implements OnInit, OnDestroy {
 
         this.contractsService.pageSize = 6;
         this.contractsService.searchFilter.clear();
+
+        if (this.isListForAdventurer) {
+          this.showOnlyMyContracts = true;
+          const index = this.ranks.indexOf(this.adventurerInfo.rank, 0);
+          this.ranks.splice(0, index);
+        }
+
         this.contractsService.refresh();
         this.contracts$ = this.contractsService.entities$;
       }
     );
+  }
+
+  set showOnlyMyContracts(value: boolean) {
+    this._showOnlyMyContracts = value;
+
+    if (this._showOnlyMyContracts) {
+      this.contractsService.searchFilter.delete('contractStatus');
+      this.contractsService.searchFilter.set('executor', this.adventurerInfo.id.toString());
+      this.minRankFilter = null;
+    } else {
+      this.contractsService.searchFilter.set('contractStatus', ContractStatus.Accepted);
+      this.contractsService.searchFilter.delete('executor');
+      this.minRankFilter = this.adventurerInfo.rank;
+    }
+  }
+
+  get showOnlyMyContracts(): boolean {
+    return this._showOnlyMyContracts;
   }
 
   set nameContractFilter(value: string) {
@@ -76,6 +110,20 @@ export class AllListContractsComponent implements OnInit, OnDestroy {
     }
   }
 
+  set rankFilter(value: Rank) {
+    this._rankFilter = value;
+
+    if (this._rankFilter) {
+      this.contractsService.searchFilter.set('rank', this._rankFilter);
+    } else {
+      this.contractsService.searchFilter.delete('rank');
+    }
+  }
+
+  get rankFilter(): Rank {
+    return this._rankFilter;
+  }
+
   set minRankFilter(value: Rank) {
     this._minRankFilter = value;
 
@@ -92,9 +140,15 @@ export class AllListContractsComponent implements OnInit, OnDestroy {
 
   clearSearch() {
     this.contractsService.searchFilter.clear();
+
+    if (this.isListForAdventurer) {
+      this.showOnlyMyContracts = this.showOnlyMyContracts;
+    } else {
+      this.contractStatusFilter = null;
+    }
+
     this.nameContractFilter = null;
-    this.minRankFilter = null;
-    this.contractStatusFilter = null;
+    this.rankFilter = null;
     this.contractsService.refresh();
   }
 
